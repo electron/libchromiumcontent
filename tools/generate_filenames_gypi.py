cbc562c7a9c6a2a56f3a6ba08652f5e475898d48
Cheng Zhao <zcbenz@gmail.com>
2015-04-02 22:12:57 +0800

    Include static libraries in dist

--- tools/generate_filenames_gypi.py
+++ tools/generate_filenames_gypi.py
@@ -17,6 +17,11 @@ SHARED_LIBRARY_SUFFIX = {
   'linux': 'so',
   'win32': 'dll',
 }[TARGET_PLATFORM]
+STATIC_LIBRARY_SUFFIX = {
+  'darwin': 'a',
+  'linux': 'a',
+  'win32': 'lib',
+}[TARGET_PLATFORM]
 
 EXCLUDE_SHARED_LIBRARIES = {
   'darwin': [],
@@ -25,23 +30,32 @@ EXCLUDE_SHARED_LIBRARIES = {
   ],
   'win32': [],
 }[TARGET_PLATFORM]
+EXCLUDE_STATIC_LIBRARIES = {
+  'darwin': [],
+  'linux': [],
+  'win32': [],
+}[TARGET_PLATFORM]
 
 GYPI_TEMPLATE = """\
 {
   'variables': {
-    'libchromiumcontent_dir': '%(src)s',
+    'libchromiumcontent_root_dir': '%(src)s',
     'libchromiumcontent_shared_libraries': %(shared_libraries)s,
+    'libchromiumcontent_static_libraries': %(static_libraries)s,
   },
 }
 """
 
 
-def main(target_file, src):
-  shared_libraries = searh_files(src, SHARED_LIBRARY_SUFFIX,
+def main(target_file, shared_src, static_src):
+  shared_libraries = searh_files(shared_src, SHARED_LIBRARY_SUFFIX,
                                  EXCLUDE_SHARED_LIBRARIES)
+  static_libraries = searh_files(static_src, STATIC_LIBRARY_SUFFIX,
+                                 EXCLUDE_STATIC_LIBRARIES)
   content = GYPI_TEMPLATE % {
     'src': os.path.dirname(target_file),
     'shared_libraries': shared_libraries,
+    'static_libraries': static_libraries,
   }
   with open(target_file, 'wb+') as f:
     f.write(content)
@@ -54,4 +68,4 @@ def searh_files(src, suffix, exclude):
 
 
 if __name__ == '__main__':
-  sys.exit(main(sys.argv[1], sys.argv[2]))
+  sys.exit(main(sys.argv[1], sys.argv[2], sys.argv[3]))
