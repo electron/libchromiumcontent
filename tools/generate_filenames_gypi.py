6b2945f386b1dae68b2b02ad7fc971f99a2f800b
Cheng Zhao <zcbenz@gmail.com>
2015-04-02 14:57:49 +0800

    Generate filenames for all platforms

--- tools/generate_filenames_gypi.py
+++ tools/generate_filenames_gypi.py
@@ -5,6 +5,27 @@ import os
 import sys
 
 
+TARGET_PLATFORM = {
+  'cygwin': 'win32',
+  'darwin': 'darwin',
+  'linux2': 'linux',
+  'win32': 'win32',
+}[sys.platform]
+
+SHARED_LIBRARY_SUFFIX = {
+  'darwin': 'dylib',
+  'linux': 'so',
+  'win32': 'dll',
+}[TARGET_PLATFORM]
+
+EXCLUDE_SHARED_LIBRARIES = {
+  'darwin': [],
+  'linux': [
+    'ffmpegsumo.so',
+  ],
+  'win32': [],
+}[TARGET_PLATFORM]
+
 GYPI_TEMPLATE = """\
 {
   'variables': {
@@ -16,14 +37,20 @@ GYPI_TEMPLATE = """\
 
 
 def main(target_file, src):
-  shared_libraries = glob.glob(os.path.join(src, '*.dylib'))
-  shared_libraries = [os.path.abspath(l) for l in shared_libraries]
+  shared_libraries = searh_files(src, SHARED_LIBRARY_SUFFIX,
+                                 EXCLUDE_SHARED_LIBRARIES)
   content = GYPI_TEMPLATE % {
     'src': os.path.dirname(target_file),
     'shared_libraries': shared_libraries,
   }
   with open(target_file, 'wb+') as f:
     f.write(content)
+
+
+def searh_files(src, suffix, exclude):
+  files = glob.glob(os.path.join(src, '*.' + suffix))
+  files = list(set(files) - set(exclude))
+  return [os.path.abspath(f) for f in files]
 
 
 if __name__ == '__main__':
