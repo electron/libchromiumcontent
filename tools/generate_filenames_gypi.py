78ecc29d283c01c469571fd29e1e2c0f84f56e27
Cheng Zhao <zcbenz@gmail.com>
2015-04-02 13:51:16 +0800

    Fix the location of filenames.gypi

--- tools/generate_filenames_gypi.py
+++ tools/generate_filenames_gypi.py
@@ -12,14 +12,13 @@ GYPI_TEMPLATE = """\
 """
 
 
-def main(src):
+def main(target_file, src):
   shared_libraries = glob.glob(os.path.join(src, '*.dylib'))
   shared_libraries = [os.path.abspath(l) for l in shared_libraries]
   content = GYPI_TEMPLATE % {'shared_libraries': shared_libraries}
-  target_file = os.path.join(src, 'libchromiumcontent_filenames.gypi')
   with open(target_file, 'wb+') as f:
     f.write(content)
 
 
 if __name__ == '__main__':
-  sys.exit(main(sys.argv[1]))
+  sys.exit(main(sys.argv[1], sys.argv[2]))
