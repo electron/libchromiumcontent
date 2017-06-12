7ee74650b376d9a6f79457228b78e053e7056e56
Kevin Sawicki <kevinsawicki@gmail.com>
2017-06-12 10:57:29 -0700

    Download chrome src using gclient

--- script/lib/config.py
+++ script/lib/config.py
@@ -4,4 +4,4 @@ import os
 
 
 def get_output_dir(source_root, target_arch, component):
-  return os.path.join(source_root, 'vendor', 'chromium', 'src', 'out-' + target_arch, component)
+  return os.path.join(source_root, 'src', 'out-' + target_arch, component)
