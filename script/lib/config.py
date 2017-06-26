75891124fad276f57c42f4c5abc4f36535aa30be
Ales Pergl <alpergl@microsoft.com>
2017-06-26 23:23:14 +0200

    Clean `src` directory in CI build

--- script/lib/config.py
+++ script/lib/config.py
@@ -4,8 +4,4 @@ import os
 
 
 def get_output_dir(source_root, target_arch, component):
-  with open(os.path.join(source_root, 'VERSION')) as f:
-    chromium_version = f.readline().strip()
-
-  return os.path.join(source_root, 'src', 'out-' + target_arch,
-                      chromium_version, component)
+  return os.path.join(source_root, 'src', 'out-' + target_arch, component)
