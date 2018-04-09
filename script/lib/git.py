788db449d42412191e60c9db3b625f36ae3b2107
Aleksei Kuzmin <alkuzmin@microsoft.com>
2018-04-09 16:25:10 +0200

    Add script/get-patch
    Use it to export commits from Chromium's and other repos as patch files.

--- script/lib/git.py
+++ script/lib/git.py
@@ -6,6 +6,8 @@ and make any assumptions about the passed arguments or calls outcomes.
 
 import subprocess
 
+from util import scoped_cwd
+
 
 def apply(repo, patch_path, reverse=False):
   args = ['git', 'apply',
@@ -22,3 +24,14 @@ def apply(repo, patch_path, reverse=False):
   applied_successfully = (return_code == 0)
 
   return applied_successfully
+
+
+def get_patch(repo, commit_hash):
+  args = ['git', 'diff-tree',
+          '-p',
+          commit_hash,
+          '--'  # Explicitly tell Git that `commit_hash` is a revision, not a path.
+          ]
+
+  with scoped_cwd(repo):
+    return subprocess.check_output(args)
