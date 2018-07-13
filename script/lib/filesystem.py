bdfcdb0fe1f4d94755e04fcf65a5767d4055d1d5
John Kleinschmidt <jkleinsc@github.com>
2018-07-13 13:40:31 -0400

    Only show download stats when verbose is used.

--- script/lib/filesystem.py
+++ script/lib/filesystem.py
@@ -45,7 +45,7 @@ def safe_unlink(path):
 def byte_to_mb(n):
   return str(n / 1024 / 1024) + 'MB'
 
-def download_and_extract(destination, url):
+def download_and_extract(destination, url, verbose):
   print url
   with tempfile.TemporaryFile() as t:
     with contextlib.closing(urllib2.urlopen(url)) as u:
@@ -57,14 +57,16 @@ def download_and_extract(destination, url):
         done += len(chunk)
         if not len(chunk):
           break
-        percent = '{0:.2f}%'.format(round(float(done) / float(total), 4) * 100)
-        ratio = '(' + byte_to_mb(done) + '/' + byte_to_mb(total) + ')'
-        line = '-> ' + percent + ' ' + ratio
-        sys.stderr.write(line.ljust(last_length) + '\r')
-        last_length = len(line)
-        sys.stderr.flush()
+        if verbose:
+          percent = '{0:.2f}%'.format(round(float(done) / float(total), 4) * 100)
+          ratio = '(' + byte_to_mb(done) + '/' + byte_to_mb(total) + ')'
+          line = '-> ' + percent + ' ' + ratio
+          sys.stderr.write(line.ljust(last_length) + '\r')
+          last_length = len(line)
+          sys.stderr.flush()
         t.write(chunk)
-    sys.stderr.write('\nExtracting...\n')
-    sys.stderr.flush()
+    if verbose:
+      sys.stderr.write('\nExtracting...\n')
+      sys.stderr.flush()
     with tarfile.open(fileobj=t, mode='r:bz2') as z:
       z.extractall(destination)
